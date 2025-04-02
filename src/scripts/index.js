76c39a338d559582285104e41f2dc88c44157176
LaisNardes <120538184+LaisNardes@users.noreply.github.com>
2025-04-02 07:38:18 +0200

    feat resolucao de codigo

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 function procuraSobremesa(sobremesas, valorDoInput) {
   /* Desenvolva sua lógica a partir aqui */
-
-  return; /* É necessário retornar uma lista de produtos */
+  return sobremesas.filter(({nome})=>
+     nome.toLowerCase().includes(valorDoInput.toLowerCase())); /* É necessário retornar uma lista de produtos */
+    
 }
